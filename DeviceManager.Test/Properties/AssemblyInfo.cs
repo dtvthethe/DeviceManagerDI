6a46dd7310ff7e74590382b47ec69a7cff183b7e
DTVThe <dtvthe@gmail.com>
2018-08-26 00:59:12 +0700

    Initial

---
+++ DeviceManager.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeviceManager.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("blogthuthuatwin10.com")]
+[assembly: AssemblyProduct("DeviceManager.Test")]
+[assembly: AssemblyCopyright("Copyright © blogthuthuatwin10.com 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59a0a71a-3585-416b-8a14-dadceb5c2599")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DeviceManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeviceManager.Web.MvcApplication" Language="C#" %>
